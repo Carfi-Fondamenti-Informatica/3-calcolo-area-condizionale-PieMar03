c0debf75f019653f2c949e91ab15eaa211e05b8c
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-11-18 08:35:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
-}
+int main(){
+        float a,b;
+        int o;
+        cin>> a >> b >> o;
+        switch (o) {
+            case 0:
+                cout<< ((a*b)/2);
+                break;
+            case 1:
+                cout<< (a*a);
+                break;
+            case 2:
+                cout << (a*b);
+                break;
+            default:
+                cout << "opzione non valida";
+        }
+        return 0;
+    }
